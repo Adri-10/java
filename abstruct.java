86a7e09c602cb62adac149a12a3e810605e2d2a2
Adri-10 <sahaadri419@gmail.com>
2020-06-04 22:19:52 +0600

    Add files via upload

---
+++ abstruct.java
@@ -0,0 +1,11 @@
+
+package new_lab_4;
+
+
+public  class abstruct extends abstruction {
+  
+  @Override
+  void sendMessege(){
+      System.out.println("Seth Rollins is a real champian of WWE");
+  }
+}
